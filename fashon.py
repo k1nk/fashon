ea6a034a556e87ee78e058d1dcadd8b552a5a37a
unknown <kenichi@nakatani.jp>
2018-07-26 09:55:56 +0900

    modified fashon.py

--- fashon.py
+++ fashon.py
@@ -9,7 +9,7 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
 from subprocess import check_output
-print(check_output(["ls", "../input"]).decode("utf8"))
+#print(check_output(["ls", "../input"]).decode("utf8"))
 
 # Any results you write to the current directory are saved as output.
 
